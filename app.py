3e1b1c733f79c08d325ebbe6db7f3179d85bc563
Vignesh1625 <eligetivignesh@gmail.com>
2024-01-01 18:58:35 +0530

    getting details from database

--- app.py
+++ app.py
@@ -126,8 +126,13 @@ def submit_form():
 
 @app.route('/teenAgeSubmit', methods=['GET', 'POST'])
 def teenAgeSubmit():
+    # Retrieve the latest user details from the database
+    latest_user = UserDetails.query.order_by(UserDetails.id.desc()).first()
+    latitude = latest_user.latitude
+    longitude = latest_user.longitude
+
     print("lat :", latitude, "long :", longitude)
-    tempature,humidity = get_weather(api_key, latitude, longitude)
+    temperature, humidity = get_weather(api_key, latitude, longitude)
     q1 = int(request.form.get('question1'))
     q2 = int(request.form.get('question2'))
     q3 = int(request.form.get('question3'))
@@ -135,15 +140,19 @@ def teenAgeSubmit():
     q5 = int(request.form.get('question5'))
     q6 = request.form.get('question6')
 
-    tempature = celsius_to_fahrenheit(tempature)
-    atmospherePrediction = atmospherePickle.predict([[humidity,tempature,q6]])
-    print("atmospherePrediction Stress Level:",atmospherePrediction)
+    temperature = celsius_to_fahrenheit(temperature)
+    atmospherePrediction = atmospherePickle.predict([[humidity, temperature, q6]])
+    print("atmospherePrediction Stress Level:", atmospherePrediction)
 
-    result = q1+q2+q3+q4+q5
-    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=session.get('name'),gender = session.get('gender'))
+    result = q1 + q2 + q3 + q4 + q5
+    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=latest_user.username, gender=latest_user.gender)
 
 @app.route('/adultAgeSubmit', methods=['GET', 'POST'])
 def adultAgeSubmit():
+    latest_user = UserDetails.query.order_by(UserDetails.id.desc()).first()
+    latitude = latest_user.latitude
+    longitude = latest_user.longitude
+
     print("lat :", latitude, "long :",  longitude)
     tempature,humidity = get_weather(api_key, latitude,  longitude)
     q1 = int(request.form.get('question1'))
@@ -158,10 +167,14 @@ def adultAgeSubmit():
     print("atmospherePrediction Stress Level:",atmospherePrediction)
 
     result = q1+q2+q3+q4+q5
-    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=session.get('name'),gender = session.get('gender'))
+    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=latest_user.username, gender=latest_user.gender)
 
 @app.route('/oldAgeSubmit', methods=['GET', 'POST'])
 def oldAgeSubmit():
+    latest_user = UserDetails.query.order_by(UserDetails.id.desc()).first()
+    latitude = latest_user.latitude
+    longitude = latest_user.longitude
+    
     print("lat :", latitude, "long :",  longitude)
     tempature,humidity = tempature,humidity = get_weather(api_key, latitude,  longitude)
     q1 = int(request.form.get('question1'))
@@ -176,8 +189,7 @@ def oldAgeSubmit():
     print("atmospherePrediction Stress Level:",atmospherePrediction)
 
     result = q1+q2+q3+q4+q5
-    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=session.get('name'),gender = session.get('gender'))
-
+    return render_template('./resultPage.html', result=result, atmospherePrediction=atmospherePrediction, Name=latest_user.username, gender=latest_user.gender)
 
 
 if __name__ == '__main__':
